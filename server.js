768b7aa155cc834e0c5343113472c1eff9f8d4da
Shane Moyo <shanemoyo@macs-MacBook-Pro.local>
2021-05-13 16:15:08 -0700

    add linter

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const cookieSession = require("cookie-session");
-const bodyParser = require("body-parser");
-const path = require('path'); 
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const app = express(); 
-
+const app = express();
 
 const port = process.env.PORT || 5000;
 
